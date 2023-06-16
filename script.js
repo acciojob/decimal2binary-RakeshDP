d24c0fad06ff7da3bfa48d773a8bb875f0b44d1e
Rakesh Pawar <125602231+RakeshDP@users.noreply.github.com>
2023-06-16 23:55:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,15 +6,16 @@
 // module.exports = threeSum;
 
 function decimalToBinary(decimal) {
-  let binary = "";
   if (decimal === 0) {
-    binary = "0";
-  } else {
-    while (decimal > 0) {
-      binary = (decimal % 2) + binary;
-      decimal = Math.floor(decimal / 2);
-    }
+    return "0";
   }
+
+  let binary = "";
+  while (decimal > 0) {
+    binary = (decimal % 2) + binary;
+    decimal = Math.floor(decimal / 2);
+  }
+
   return binary;
 }
 
